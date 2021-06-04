b669eef91fc46c344d961256321c86484283ab8b
denosy <50819494+denosy@users.noreply.github.com>
2021-06-04 13:46:36 +0300

    share

---
+++ src/componenets/header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./_header.scss"
+export default function Header() {
+    return (
+        <div>
+           Header 
+        </div>
+    )
+}
---
+++ src/componenets/sidebar/SideBar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./_sidebar.scss";
+
+export default function SideBar() {
+    return (
+        <div>
+            sidebar
+        </div>
+    )
+}
