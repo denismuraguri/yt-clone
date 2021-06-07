cf9652d8a6171d570feb409c623c3faa60bf9c27
denosy <50819494+denosy@users.noreply.github.com>
2021-06-07 09:49:52 +0300

    share

--- src/App.js
+++ src/App.js
@@ -3,6 +3,70 @@ import { Container } from 'react-bootstrap'
 import Header from './componenets/header/Header'
 import SideBar from './componenets/sidebar/SideBar'
 import HomeScreen from './screens/homescreen/HomeScreen'
+import LoginScreen from './screens/loginscreens/LoginScreen'
+import {BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom"
+
+
+import "./_app.scss";
+
+const Layout = ({children}) =>{
+    const [sidebar, toogleSidebar] = useState(false)
+
+    const handleToggleSidebar = ()=> toogleSidebar(value=>!value)
+
+    return(
+        <>
+            <Header handleToggleSidebar={handleToggleSidebar} />
+            <div className="app__container border border-info">
+                <SideBar 
+                sidebar={sidebar}
+                handleToggleSidebar={handleToggleSidebar}
+                 />
+                <Container fluid className="app__main border border-warning">
+                    {children}
+                </Container>
+            </div>
+        </>
+
+    )
+
+}
+
+export default function App() {
+    
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <Layout>
+                        <HomeScreen />
+
+                    </Layout>
+                </Route>
+                <Route path="/auth">
+                    <LoginScreen />
+                </Route>
+                <Route path="/search">
+                    <Layout>
+                        <h1>Search Result</h1>
+                    </Layout>
+                </Route>
+                <Route>
+                    <Redirect to="/" />
+                </Route>
+            </Switch>
+        </Router>
+
+        //<LoginScreen />
+    )
+}
+ 
+
+/*import React, { useState } from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './componenets/header/Header'
+import SideBar from './componenets/sidebar/SideBar'
+import HomeScreen from './screens/homescreen/HomeScreen'
 
 import "./_app.scss";
 
@@ -25,3 +89,4 @@ export default function App() {
         </div>
     )
 }
+*/
---
+++ src/screens/loginscreens/LoginScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./_login.scss"
+
+export default function LoginScreen() {
+    return (
+        <div className="login">
+            <div className="login__container">
+                <img src="http://pngimg.com/uploads/youtube/youtube_PNG2.png" alt="" />
+                <button>Login with google</button>
+                <p>This project is made using YOUTUBE DATA API</p>
+            </div>
+        </div>
+    )
+}
