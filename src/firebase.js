4dac6449ce2d67b95d4b87a791f448d8b5bd7210
denosy <50819494+denosy@users.noreply.github.com>
2021-06-07 14:12:37 +0300

    share

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBgpLRHfGbhdIzkyVZ6ieo4QfK2LFGVf0I",
+    authDomain: "yt-clone-proj.firebaseapp.com",
+    projectId: "yt-clone-proj",
+    storageBucket: "yt-clone-proj.appspot.com",
+    messagingSenderId: "1078865314633",
+    appId: "1:1078865314633:web:fee2b8b7b18ccf57211098"
+};
+
+firebase.initializeApp(firebaseConfig)
