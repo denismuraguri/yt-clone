46da5366461b8bcb3d2a29fb5da848ce695accd1
denosy <50819494+denosy@users.noreply.github.com>
2021-06-11 10:23:55 +0300

    share

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import ReactDom from 'react-dom';
 import React from "react"
 import App from "./App";
+import { Provider } from "react-redux"
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import "./_base.scss"
+import store from './redux/store';
 
-ReactDom.render(<App />, document.getElementById("root"))
+ReactDom.render(
+    <Provider store={store}>
+        <App />,
+    </Provider>, document.getElementById("root"))
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,16 @@
-import {create}
+import {createStore, applyMiddleware} from "redux"
+
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from "redux-thunk"
+const initialState = {
+    name: "summit",
+    age: "21",
+}
+const reducer = (initialState) => initialState
+const store = createStore(reducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(thunk))
+)
+
+export default store;
+
